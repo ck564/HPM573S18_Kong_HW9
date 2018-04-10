556c86f5d120e207ffff98d3480d9ee06844ef18
ckong <calvin.kong@yale.edu>
2018-04-10 16:45:23 -0400

    update

--- RunMarkov.py
+++ RunMarkov.py
@@ -3,17 +3,18 @@ import Parameters as Parameters
 import SupportMarkovModel as Support
 import scr.SamplePathClasses as PathClasses
 
+# no drug
 cohort1 = MarkovClasses.Cohort(id=1, therapy=Parameters.Therapies.no_drug)
 simOutputs1 = cohort1.simulate()
 Support.print_outcomes(simOutputs1, "No drug")
 
-
+# drug treatment
 cohort2 = MarkovClasses.Cohort(id=1, therapy=Parameters.Therapies.tx_drug)
 simOutputs2 = cohort2.simulate()
 Support.print_outcomes(simOutputs2, "Drug Treatment")
 
 
-# survival curve
+# survival curves
 PathClasses.graph_sample_path(
     sample_path=simOutputs1.get_survival_curve(),
     title='Survival curve (no drug)',
@@ -27,5 +28,7 @@ PathClasses.graph_sample_path(
     x_label='Simulation time step',
     y_label='Number of alive individuals'
 )
+
+
 
 
